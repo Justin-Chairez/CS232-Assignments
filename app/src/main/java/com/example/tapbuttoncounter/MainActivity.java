ad0ff37484409ded315f1f9e3922cc4b8fee01a2
Justin Chairez <jtchairez@gmail.com>
2021-01-25 13:05:57 -0800

    Delete .idea directory
    (cherry picked from commit 58e24bd78702e5c25ce0038c0d07b8c646197c2c)

---
+++ app/src/main/java/com/example/tapbuttoncounter/MainActivity.java
@@ -0,0 +1,47 @@
+
+package com.example.tapbuttoncounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //1.Reference global data, models, and views
+    private Counter count;
+    private TextView countView;
+    private Button tapBtn;
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+        //2. CREATE THE DATA MODEL
+        count = new Counter();
+
+        //3. REFERENCE THE VIEWS
+        countView = findViewById(R.id.textView);
+        tapBtn = findViewById(R.id.button);
+
+        //4. REGISTER A LISTENER EVEN FOR THE BUTTON - USE A HANDLER
+        tapBtn.setOnClickListener(updateCounter);
+    }
+
+    //5. HANDLER - updateCounter()
+    private final View.OnClickListener updateCounter = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            count.addCount();
+            countView.setText(count.getCount().toString());
+            Log.i(TAG, "Justin's Count: " + count.getCount().toString());
+        }
+    };
+}
